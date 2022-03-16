02630dd420166bbdc46a2b805d3a23bf20178135
Pablo Vila Mauricio <pvilamauricio@danielcastelao.org>
2022-03-16 14:09:00 +0100

    Main avisando de que es el último commit para posteriori hacer el merge squash

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Este es el commit inicial");
+        System.out.println("Este es el commit final");
     }
 }
