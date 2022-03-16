8406d09073730d8d703a1d2245f11117fb55a434
Pablo Vila Mauricio <pvilamauricio@danielcastelao.org>
2022-03-16 14:27:54 +0100

    Final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Este es el commit final");
+        System.out.println("Este es el commit final V.2 (creo que los commits y mi duda en clase ya explica lo acontecido y el porque no seççesta exactamente como se solicita");
     }
 }
